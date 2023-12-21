31ad5704e925d1983a86dfddedae46f9e07f0c6a
eprayoga <endangprayoga95@gmail.com>
2023-12-22 00:35:32 +0700

    Fix bug MBTI result

--- app/src/main/java/com/dicoding/kenari/view/mbti/Test10Activity.kt
+++ app/src/main/java/com/dicoding/kenari/view/mbti/Test10Activity.kt
@@ -3,22 +3,17 @@ package com.dicoding.kenari.view.mbti
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.dicoding.kenari.R
 import com.dicoding.kenari.api.ApiConfig
 import com.dicoding.kenari.api.MbtiResultRequest
 import com.dicoding.kenari.api.MbtiResultResponse
-import com.dicoding.kenari.api.ModelResponse
-import com.dicoding.kenari.api.SaveChatResponse
-import com.dicoding.kenari.api.SaveChatResponseRequest
 import com.dicoding.kenari.databinding.ActivityTest10Binding
 import com.dicoding.kenari.view.ViewModelFactory
-import com.dicoding.kenari.view.discussion.DiscussionActivity
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
@@ -30,64 +25,86 @@ class Test10Activity : AppCompatActivity() {
         ViewModelFactory.getInstance(this)
     }
     private lateinit var binding: ActivityTest10Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test10)
         binding = ActivityTest10Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportActionBar?.title = "Tes MBTI"
-        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#20BAB3")))
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setupActionBar()
 
         viewModel.getSession().observe(this) { user ->
-            lifecycleScope.launch {
-                ApiConfig.initialize(user.token)
-            }
+            initializeApiConfig(user.token)
         }
 
         binding.next.setOnClickListener {
-            if (binding.C036.isChecked) {
-                viewModel.addMBTI("C036")
-            }
-            if (binding.C049.isChecked) {
-                viewModel.addMBTI("C049")
-            }
-            if (binding.C057.isChecked) {
-                viewModel.addMBTI("C057")
-            }
-            viewModel.getSelectedMBTI().observe(this) { selectedMBTI ->
-                val selectedCharacter = MbtiResultRequest(selectedMBTI.distinct())
+            handleNextButtonClick()
+        }
+    }
 
-                ApiConfig.instanceRetrofit.mbtiResult(selectedCharacter)
-                    .enqueue(object : Callback<MbtiResultResponse> {
-                        override fun onResponse(call: Call<MbtiResultResponse>, response: Response<MbtiResultResponse>) {
+    private fun setupActionBar() {
+        supportActionBar?.title = "Tes MBTI"
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#20BAB3")))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 
-                            if (response.isSuccessful) {
-                                val responseBody = response.body()
+    private fun initializeApiConfig(token: String) {
+        lifecycleScope.launch {
+            ApiConfig.initialize(token)
+        }
+    }
 
-                                if (responseBody != null) {
-                                    val personality = responseBody.data.typeResult.type
-                                    val info = responseBody.data.typeResult.information
+    private fun handleNextButtonClick() {
+        if (binding.C036.isChecked) {
+            viewModel.addMBTI("C036")
+        }
+        if (binding.C049.isChecked) {
+            viewModel.addMBTI("C049")
+        }
+        if (binding.C057.isChecked) {
+            viewModel.addMBTI("C057")
+        }
 
-                                    viewModel.resetMBTI()
+        var selectedCharacter: MbtiResultRequest? = null
 
-                                    val intent = Intent(this@Test10Activity, MbtiResultActivity::class.java)
-                                    intent.putExtra("personality", personality)
-                                    intent.putExtra("info", info)
-                                    startActivity(intent)
-                                    finish()
-                                }
+        viewModel.getSelectedMBTI().observe(this) { selectedMBTI ->
+            selectedCharacter = MbtiResultRequest(selectedMBTI.distinct())
+        }
+
+        Toast.makeText(this@Test10Activity, "Tes telah selesai, berikut hasilnya!", Toast.LENGTH_SHORT).show()
+
+        // Cek apakah selectedCharacter sudah diinisialisasi atau belum
+        if (selectedCharacter != null) {
+            ApiConfig.instanceRetrofit.mbtiResult(selectedCharacter!!)
+                .enqueue(object : Callback<MbtiResultResponse> {
+                    override fun onResponse(call: Call<MbtiResultResponse>, response: Response<MbtiResultResponse>) {
+
+                        if (response.isSuccessful) {
+                            val responseBody = response.body()
+
+                            if (responseBody != null) {
+                                val personality = responseBody.data.typeResult.type
+                                val info = responseBody.data.typeResult.information
+
+                                viewModel.resetMBTI()
+
+                                val intent = Intent(this@Test10Activity, MbtiResultActivity::class.java)
+                                intent.putExtra("personality", personality)
+                                intent.putExtra("info", info)
+                                startActivity(intent)
+                                finish()
                             }
                         }
+                    }
 
-                        override fun onFailure(call: Call<MbtiResultResponse>, t: Throwable) {
-                            // Handle failure here if needed
-                            Log.e("ChatbotActivity", "Error", t)
-                            Toast.makeText(this@Test10Activity, "Tidak dapat terhubung ke server", Toast.LENGTH_SHORT).show()
-                        }
-                    })
-            }
+                    override fun onFailure(call: Call<MbtiResultResponse>, t: Throwable) {
+                        // Handle failure here if needed
+                        Log.e("ChatbotActivity", "Error", t)
+                        Toast.makeText(this@Test10Activity, "Tidak dapat terhubung ke server", Toast.LENGTH_SHORT).show()
+                    }
+                })
+        } else {
+            Toast.makeText(this@Test10Activity, "Gagal mengambil data, silahkan ulangi tes!", Toast.LENGTH_SHORT).show()
         }
     }
 }
