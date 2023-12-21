b1138a2c35642183fd65484e0ecbe34f7cbe5ac6
eprayoga <endangprayoga95@gmail.com>
2023-12-22 01:24:08 +0700

    Change about page

--- app/src/main/java/com/dicoding/kenari/view/about/AboutActivity.kt
+++ app/src/main/java/com/dicoding/kenari/view/about/AboutActivity.kt
@@ -4,16 +4,38 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.dicoding.kenari.R
+import com.dicoding.kenari.api.ApiConfig
+import com.dicoding.kenari.databinding.ActivityAboutBinding
+import com.dicoding.kenari.view.ViewModelFactory
+import com.dicoding.kenari.view.mbti.MbtiViewModel
+import kotlinx.coroutines.launch
 
 class AboutActivity : AppCompatActivity() {
+    private val viewModel by viewModels<MbtiViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+    private lateinit var binding: ActivityAboutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        supportActionBar?.title = "About"
+        supportActionBar?.title = "Akun Saya"
         supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#20BAB3")))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        viewModel.getSession().observe(this) { user ->
+            binding.textViewName.text = user.name
+            binding.textViewEmail.text = user.email
+
+            lifecycleScope.launch {
+                ApiConfig.initialize(user.token)
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
